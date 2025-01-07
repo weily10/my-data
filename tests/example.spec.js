140b508bed2a0edc60f818009343c80bcf89b077
weily10 <w3ily10@gmail.com>
2025-01-07 15:20:35 +0800

    first test

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,19 +1,8 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('should display homepage', async ({page})=>{
+  await page.goto('http://localhost:5173/')
+  await expect(page).toHaveTitle(/My App/i);
+})
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
