c2964dd2e865bc23082e6249abe4a461267f90e8
weily10 <w3ily10@gmail.com>
2025-01-07 15:52:56 +0800

    add title

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -3,6 +3,6 @@ import { test, expect } from '@playwright/test';
 
 test('should display homepage', async ({page})=>{
   await page.goto('http://localhost:5173/')
-  await expect(page).toHaveTitle(/My App/i);
+  await expect(page).toHaveTitle(/MyData/i);
 })
 
