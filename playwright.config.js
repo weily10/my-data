9a2adfb62985d40d2622b454b0f0dbff0f9497e4
weily10 <w3ily10@gmail.com>
2025-01-07 16:14:24 +0800

    change yml

--- playwright.config.js
+++ playwright.config.js
@@ -11,6 +11,7 @@ import { defineConfig,devices } from '@playwright/test';
  * @see https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
+  timeout: 50000,
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
