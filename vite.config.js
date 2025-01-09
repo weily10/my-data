c3a96d188897a68313f48e540d5f910cc5ae0f41
weily huang <weily.huang@ssmolab.com>
2025-01-09 21:58:11 +0800

    vite config change

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,29 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig, loadEnv } from "vite";
+import vue from "@vitejs/plugin-vue";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  base: process.env.VITE_BASE_URL,
-  server: {
-    watch: {
-      usePolling: true,
-    },
-    proxy: {
-      '/apiPlatform/': {
-        target: 'http://10.201.201.22:8080',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/apiPlatform/, '/api/'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  
+  return {
+    plugins: [vue()],
+    base: env.VITE_BASE_URL || "/",
+    server: {
+      watch: {
+        usePolling: true,
       },
-      '/api/': {
-        target: 'http://10.201.201.22:8081',
-      }
-      // '/api-dp2/': 'http://10.201.201.22:8082',
-      // '/api-dp3/': 'http://10.201.201.22:8083',
-    }
-  },
-})
+      proxy: {
+        "/apiPlatform/": {
+          target: "http://10.201.201.22:8080",
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/apiPlatform/, "/api/"),
+        },
+        "/api/": {
+          target: "http://10.201.201.22:8081",
+        },
+        // '/api-dp2/': 'http://10.201.201.22:8082',
+        // '/api-dp3/': 'http://10.201.201.22:8083',
+      },
+    },
+  };
+});
