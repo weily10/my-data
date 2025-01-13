0f4338c23984ff801a8e8362d341916ccd099c60
weily10 <w3ily10@gmail.com>
2025-01-13 13:49:17 +0800

    add title

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,33 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
+export default defineConfig({
 
-  
-  return {
-    plugins: [vue()],
-    base: env.VITE_BASE_URL || "/",
-    server: {
-      watch: {
-        usePolling: true,
-      }
-      // proxy: {
-      //   "/apiPlatform/": {
-      //     target: "http://10.201.201.22:8080",
-      //     changeOrigin: true,
-      //     rewrite: (path) => path.replace(/^\/apiPlatform/, "/api/"),
-      //   },
-      //   "/api/": {
-      //     target: "http://10.201.201.22:8081",
-      //   },
-      //   // '/api-dp2/': 'http://10.201.201.22:8082',
-      //   // '/api-dp3/': 'http://10.201.201.22:8083',
-      // },
-    },
-  };
+
+  plugins: [vue(),
+    createHtmlPlugin({
+      inject: {
+        data: {
+          title: process.env.VITE_APP_TITLE || 'Default Title',
+        },
+      },
+    })],
+  base: env.VITE_BASE_URL || "/",
+  server: {
+    watch: {
+      usePolling: true,
+    }
+    // proxy: {
+    //   "/apiPlatform/": {
+    //     target: "http://10.201.201.22:8080",
+    //     changeOrigin: true,
+    //     rewrite: (path) => path.replace(/^\/apiPlatform/, "/api/"),
+    //   },
+    //   "/api/": {
+    //     target: "http://10.201.201.22:8081",
+    //   },
+    //   // '/api-dp2/': 'http://10.201.201.22:8082',
+    //   // '/api-dp3/': 'http://10.201.201.22:8083',
+    // },
+  }
 });
