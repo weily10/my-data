a86e9e36b2a92948704b7a479f717461bb3d29f7
weily10 <w3ily10@gmail.com>
2025-01-13 11:53:42 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => {
   
   return {
     plugins: [vue()],
-    base: "/",
+    base: env.VITE_BASE_URL || "/",
     server: {
       watch: {
         usePolling: true,
